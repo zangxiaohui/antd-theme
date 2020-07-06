ee42d28130e799a962bed7be253b08c0b1531a90
Wang Mengzhe <wangmengzhe@maxtropy.com>
2020-07-06 18:13:29 +0800

    remove webpack build rawcss file for direct inclusion by cra

---
+++ build.js
@@ -0,0 +1,38 @@
+const fs =require("fs");
+const path = require("path");
+const less = require("less");
+const NpmImportPlugin = require("less-plugin-npm-import");
+const sources = {
+    "./src/themes/dark/antd.less": [
+        "antd-dark.css",
+        "antd-dark.rawcss",
+    ],
+    "./src/themes/light/antd.less": [
+        "antd-light.css",
+        "antd-light.rawcss",
+    ],
+}
+
+const options = {
+    javascriptEnabled: true,
+    plugins: [
+        new NpmImportPlugin({prefix: "~"})
+    ]
+};
+
+const promises = Object.keys(sources).map( async src => {
+    const lessContent = await fs.promises.readFile(src, {encoding: "utf8"});
+    const rendered = await less.render(lessContent, {
+        ...options,
+        paths: [...options.paths || [], path.dirname(src)]
+    });
+    await Promise.all(sources[src].map(dest => fs.promises.writeFile(dest, rendered.css)));
+});
+
+Promise.all(promises).then(() => process.exit(0))
+    .catch(err => {
+        console.log(err);
+        process.exit(1);
+    });
+
+setInterval(() => {}, 1000);
